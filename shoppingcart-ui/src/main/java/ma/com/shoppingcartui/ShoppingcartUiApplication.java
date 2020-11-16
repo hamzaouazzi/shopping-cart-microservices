39d66f684020c1dbac0cd07539cbb7dd432d3f1c
HAMZA OUAZZI <ouazzihamza97@gmail.com>
2020-11-16 19:32:31 +0100

    implemented API Gateway using Spring Cloud Zuul Proxy

---
+++ shoppingcart-ui/src/main/java/ma/com/shoppingcartui/ShoppingcartUiApplication.java
@@ -0,0 +1,15 @@
+package ma.com.shoppingcartui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ShoppingcartUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingcartUiApplication.class, args);
+    }
+
+}
---
+++ shoppingcart-ui/src/test/java/ma/com/shoppingcartui/ShoppingcartUiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.com.shoppingcartui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
